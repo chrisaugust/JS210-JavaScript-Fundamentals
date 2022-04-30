f2b5e7b52662441824465c55922a1585e4a681e5
Chris Danielson <christopher.a.danielson@protonmail.com>
2022-04-29 20:48:26 -0600

    Expensive Study Preparation

---
+++ shoppingCart.js
@@ -0,0 +1,81 @@
+// The shopping cart is a list of items, where each item
+// is an object { name: <string>, amount: <number> }.
+let shoppingCart = [];
+
+// Currently available products with prices.
+const prices = {
+  'notebook': 9.99,
+  'pencil': 1.70,
+  'coffee': 3.00,
+  'smoothie': 2.10,
+};
+
+function price({name}) {
+  return prices[name];
+}
+
+// Adding an item to the shopping cart.
+// The amount is optional and defaults to 1.
+// If the item is already in the cart, its amount is updated.
+function updateCart(name, amount = 1) {
+  // the problem with the original line of code that follows is that
+  // if 0 is passed in as argument, as a falsy value it will always
+  // result in amount be assigned to 1, which is not what we want.
+  // We could use a ternary statement as below, or have a default 
+  // value for the parameter 
+  // amount = amount || 1;
+  //
+  // possible solution:
+  // amount = amount === undefined ? 1 : amount;
+
+  let alreadyInCart = false;
+
+  for (let i = 0; i < shoppingCart.length; i += 1) {
+    let item = shoppingCart[i];
+
+    if (item.name === name) {
+      item.amount = amount;
+      alreadyInCart = true;
+      break;
+    }
+  }
+
+  if (!alreadyInCart) {
+    shoppingCart.push({ name, amount });
+  }
+}
+
+// Calculating the price for the whole shopping cart.
+function total() {
+  let total = 0;
+
+  for (let i = 0; i < shoppingCart.length; i += 1) {
+    let item = shoppingCart[i];
+
+    total += (item.amount * price(item));
+  }
+
+  return total.toFixed(2);
+}
+
+function pay() {
+  // omitted
+
+  console.log(`You have been charged $${total()}.`);
+}
+
+function checkout() {
+  pay();
+  shoppingCart = [];
+}
+
+// Example purchase.
+
+updateCart('notebook');
+updateCart('pencil', 2);
+updateCart('coffee', 1);
+// "Oh, wait, I do have pencils..."
+updateCart('pencil', 0);
+
+checkout();
+// You have been charged $14.69.
